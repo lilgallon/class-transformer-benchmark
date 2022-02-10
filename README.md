7a29114f8a827414a65a14af8b17a0467ad2b30d
Gallon Lilian <lilian@gallon.dev>
2022-02-10 22:22:45 +0100

    improved benchmark

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
 # class-transformer-benchmark
 typestack/class-transformer benchmark
-
-- `npm install -g typescript`
-- `tsc *.ts`
--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {plainToClass, Type} from "class-transformer";
+import {plainToInstance, Type} from "class-transformer";
 import 'reflect-metadata';
+import {HttpClient} from "@angular/common/http";
 
 function timeFrom(date: Date): string {
   return (new Date().getTime() - date.getTime()) / 1000 + " s"
@@ -36,23 +37,61 @@ class Airline {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  public fetchingTime: string = 'loading';
+  public passengers: Passenger[] = [];
   public parsingTimeWithoutClassTransformer: string = 'waiting';
   public parsingTimeWithClassTransformer: string = 'waiting';
+  public size: string = '?';
+
+  constructor(private httpClient: HttpClient) {
+  }
 
   ngOnInit(): void {
-    let start = new Date();
-    fetch('https://api.instantwebtools.net/v1/passenger', {headers: {'mode': 'no-cors'}})
-      .then((res: Response) => {
-        start = new Date();
-        this.parsingTimeWithoutClassTransformer = 'loading';
-        res.json().then(() => this.parsingTimeWithoutClassTransformer = timeFrom(start));
 
-        start = new Date();
-        this.parsingTimeWithClassTransformer = 'loading';
-        const passengers = plainToClass(Passenger, res.body)
-        this.parsingTimeWithClassTransformer = timeFrom(start);
-      })
-      .catch((error) => console.error(error));
+    this.httpClient
+      .get<{ data: Passenger[] }>('https://api.instantwebtools.net/v1/passenger', {headers: { 'Access-Control-Allow-Origin': '*' }})
+      .subscribe((passengersResult) => {
+        const passengers = passengersResult.data;
+
+        const evenMorePassengers = [
+          ...passengers,
+          ...passengers,
+          ...passengers,
+          ...passengers,
+          ...passengers,
+          ...passengers,
+          ...passengers,
+          ...passengers,
+          ...passengers,
+          ...passengers,
+          ...passengers,
+          ...passengers,
+          ...passengers,
+          ...passengers,
+          ...passengers,
+          ...passengers,
+          ...passengers,
+          ...passengers,
+          ...passengers,
+          ...passengers,
+          ...passengers,
+          ...passengers,
+          ...passengers
+        ]
+
+        let start1 = new Date();
+        this.passengers = plainToInstance(Passenger, evenMorePassengers);
+        this.parsingTimeWithClassTransformer = timeFrom(start1);
+
+        let start2 = new Date();
+        this.passengers = evenMorePassengers;
+        this.parsingTimeWithoutClassTransformer = timeFrom(start2);
+
+        this.size = (this.sizeof(evenMorePassengers) / 1000000) + ' MB';
+      });
+  }
+
+  // bytes
+  private sizeof(o: any): number {
+    return JSON.stringify(o).replace(/[\[\]\,\"]/g,'').length
   }
 }
